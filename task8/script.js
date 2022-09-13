c2bdb8afa84f659f6db466c4633d45f0c24955ab
adityasinha198 <adityasinha198@gmail.com>
2022-09-13 23:02:45 +0530

    task11

--- task8/script.js
+++ task8/script.js
@@ -1,60 +1,45 @@
-const cart_items = document.querySelector('#cart .cart-items');
 
+window.addEventListener('DOMContentLoaded', (e) => {
+    e.preventDefault()
+    axios.get("http://localhost:3000/products")
+    .then((res) => {
+        const song = document.getElementById("music-content")
+        for(let i=0;i<res.data.length ; i++){
+        console.log(res.data[0])
+        let info= res.data[i]
+        const ID = info.id
+        const name = info.title
+        const price = info.price
+        const img_url = info.imageUrl
+        
+        const listen = document.createElement('div')
 
-const parentContainer = document.getElementById('EcommerceContainer');
-parentContainer.addEventListener('click',(e)=>{
+        listen.setAttribute("id",ID)
+        console.log(listen)
+        
+        listen.innerHTML  = 
+        
+        `<h3>${name}</h3>
+        <div class="image-container">
+            <img class="prod-images" src=""
+            alt="">
+        </div>
+        <div class="prod-details">
+            
+             <span>$<span>${price}  </span></span>
+           
+            <button class="shop-item-button" type="button">ADD TO CART</button>
+        
+            
+        </div>
 
-    if (e.target.className=='shop-item-button'){
-        const id = e.target.parentNode.parentNode.id
-        console.log(id)
-        const name = document.querySelector(`#${id} h3`).innerText;
-        console.log(name)
-        const img_src = document.querySelector(`#${id} img`).src;
-        console.log(img_src)
-        const price = e.target.parentNode.firstElementChild.firstElementChild.innerText;
-        console.log(price)
-        let total_cart_price = document.querySelector('#total-value').innerText;
-        console.log(total_cart_price)
-        if (document.querySelector(`#in-cart-${id}`)){
-            alert('This item is already added to the cart');
-            return
+    </div>`
+  song.appendChild(listen)
         }
-        document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)+1
-        const cart_item = document.createElement('div');
-        cart_item.classList.add('cart-row');
-        cart_item.setAttribute('id',`in-cart-${id}`);
-        total_cart_price = parseFloat(total_cart_price) + parseFloat(price)
-        total_cart_price = total_cart_price.toFixed(2)
-        document.querySelector('#total-value').innerText = `${total_cart_price}`;
-        cart_item.innerHTML = `
-        <span class='cart-item cart-column'>
-        <img class='cart-img' src="${img_src}" alt="">
-            <span>${name}</span>
-    </span>
-    <span class='cart-price cart-column'>${price}</span>
-    <span class='cart-quantity cart-column'>
-        <input type="text" value="1">
-        <button>REMOVE</button>
-    </span>`
-    console.log(cart_item)
-        cart_items.appendChild(cart_item)
-    
-    
-    const container = document.getElementById('container');
-    const notification = document.createElement('div');
-    notification.classList.add('notification');
-    notification.innerHTML = `<h4>Your Product : <span>${name}</span> is added to the cart<h4>`;
-    container.appendChild(notification);
-    setTimeout(()=>{
-        notification.remove();
-    },2500)
-}
-if (e.target.className=='cart-btn-bottom' || e.target.className=='cart-bottom' || e.target.className=='cart-holder'){
-    document.querySelector('#cart').style = "display:block;"
-}
-if (e.target.className=='cancel'){
-    document.querySelector('#cart').style = "display:none;"
-}
-    
-}
-)
+        
+
+        
+
+    })
+   
+});
